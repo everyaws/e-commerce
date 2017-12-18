25962bc69256ceb65ef5fc033aff1bce0b18e12d
MOHAMMED ESAFI <everyaws@gmail.com>
2017-12-18 06:50:17 +0000

    First Commit

---
+++ admin/includes/templates/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Control Panel</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+		<script defer src="https://use.fontawesome.com/releases/v5.0.1/js/all.js"></script>
+		<link rel="stylesheet" href="<?php echo $css; ?>style.css">
+	</head>
+	<body>
---
+++ admin/init.php
@@ -0,0 +1,5 @@
+<?php
+	// Routs
+	$tpl = 'includes/templates/'; //Template Directory
+	$css = 'layout/css/'; //CSS Directory
+	$js = 'layout/js/'; //JS Directory
